62428a1f178d459df78a36e29a7ea48a8e3bedca
Zihan Chen <zihan.chen.jhu@gmail.com>
2024-07-02 07:25:30 +0800

    added README file

---
+++ README.md
@@ -0,0 +1,13 @@
+## About
+Put each page of a PDF file (from a scanner) as an image into a WORD document
+
+## How to setup
+Requirements:
+```commandline
+pip install -r requirements.txt
+```
+
+Usage:
+1. download poppler from https://poppler.freedesktop.org/
+2. save to the same folder, script uses ./poppler/bin as default path
+3. run, select a PDF file => word docx
--- pdf2word.py
+++ pdf2word.py
@@ -3,13 +3,8 @@
 """
 Put each page of a PDF file (from a scanner) as an image into a WORD document
 
-Requirements:
-pip install -r requirements.txt
-
-Usage:
-1 - download poppler from https://poppler.freedesktop.org/
-2 - save to the same folder, script uses ./poppler/bin as default path
-3 - run, select a PDF file => word docx
+Author: Zihan Chen
+Date: 2022-09-18
 """
 
 import docx
